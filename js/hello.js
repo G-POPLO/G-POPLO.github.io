4cb371c6988950c14b6bc6f98c57c469a7c30d56
unknown <CPoplo@outlook.com>
2021-06-13 23:14:07 +0800

    Site updated: 2021-06-13 23:14:06

---
+++ js/hello.js
@@ -0,0 +1,4 @@
+console.log("等等，你为什么要点进这里？");
+console.log("其实···这里没有什么好看的啊");
+console.info("嗯···如果你想看看这里有什么东西的话，或许你可以点击这个链接？")
+console.info("%chttps://lab.wwwcpoplo.top/hide", "color:blue;font-weight:bold;")
